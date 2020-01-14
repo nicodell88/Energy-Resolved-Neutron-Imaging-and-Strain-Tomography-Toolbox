79b7297b1a4578cbb0cbde551783a46038ffc7a2
Nic O'Dell <nicodell88@gmail.com>
2020-01-14 11:41:49 +1100

    Added cross correlation method, example uses al111 edge

---
+++ crossCorrMethod.m
@@ -0,0 +1,138 @@
+function [deltaD,sigma] = crossCorrMethod(Tr1,Tr2,tof,opts)
+%crossCorrMethod Calculates the "shift" in wavelength or time of flight
+%	between two sets of transmission data. Through an unstressed sample and a
+%	stressed sample. This method is presented in: Ramadhan, R.S., Kockelmann,
+%	W., Minniti, T., Chen, B., Parfitt, D., Fitzpatrick, M.E. and Tremsin,
+%	A.S., 2019. Characterization and application of Bragg-edge transmission
+%	imaging for strain measurement and crystallographic analysis on the IMAT
+%	beamline. Journal of Applied Crystallography, 52(2).
+%	https://journals.iucr.org/j/issues/2019/02/00/ks5612/ks5612.pdf
+%
+% Inputs:
+%   - Tr1 is a Nx1 double containing raw transmisssion curve
+%   for a single projection (stresseed)
+% 	- Tr2 is a Nx1 double containing the raw transmisssion curve
+%   for a single projection (unstressed)
+%   - tof is an Nx1 array of wave-lengths or time-of-flight.
+%   - options is a structure containing
+%       opts.range      :   a 2 element vector contianing the window for
+%                           fitting [start end] in the same units as tof.
+%       opts.order      :   order for SGOLAY filter
+%       opts.frame      :   framelength for SGOLAY filter
+%       opts.peakWindow :   sample window for peak fitting voigt function.
+%
+% Outputs:
+%	- deltaD is shift between the Bragg-Edge in the un-stressed and
+%       stressed sample.
+%   - sigma is the estimated standard deviation
+%
+% Copyright (C) 2020 The University of Newcastle, Australia
+% Authors:
+%   Nicholas O'Dell <Nicholas.Odell@newcastle.edu.au>
+% Last modified: 14/01/2020
+% This program is licensed under GNU GPLv3, see LICENSE for more details.
+
+
+Tr1 = Tr1(:);
+Tr2 = Tr2(:);
+tof = tof(:);
+%% Curve fitting options for fitting voigt function
+optionsFit              = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt');
+optionsFit.Algorithm    = 'Levenberg-Marquardt';
+optionsFit.Jacobian     = 'off';
+optionsFit.Display      = 'off';
+optionsFit.OptimalityTolerance = 1e-6;
+
+%% populate opts structure
+if nargin>3
+    if ~isfield(opts,'range')
+        opts.range = [tof(1) tof(end)];
+    end
+    
+    if ~isfield(opts,'order')
+        opts.order = 3;
+    end
+    
+    if ~isfield(opts,'frame')
+        opts.frame = 9;
+    end
+    if ~isfield(opts,'peakWindow')
+        opts.peakWindow = 20;
+    end
+    
+    if ~isfield(opts,'p00')
+        opts.p00 = [  ...
+            15.2407     %y0
+            0.0         %xc
+            1.8534      %log(A)
+            -4.1025     %log(Wg)
+            -3.5159     %log(Wl)
+            0.0876];    %log(Mu)
+    end
+else
+    opts.range = [tof(1) tof(end)];
+    opts.order = 5;
+    opts.frame = 7;
+    opts.peakWindow = 20;
+    
+    opts.p00 = [  ...
+        15.2407     %y0
+        0.0         %xc
+        1.8534      %log(A)
+        -4.1025     %log(Wg)
+        -3.5159     %log(Wl)
+        0.0876];    %log(Mu)
+end
+
+
+%% Inspect data
+[~,opts.fitIdx] = min((tof(:).'-opts.range(:)).^2,[],2);
+if any(abs(diff(diff(tof(opts.fitIdx(1):opts.fitIdx(2))))) > 1e-5)
+    warning('Data must be uniformly sampled.')
+end
+
+%% Smooth data
+[~,g] = sgolay(opts.order,opts.frame);
+
+p = 1;  %1st derivative
+dt = tof(opts.fitIdx(2))-tof(opts.fitIdx(2)-1);
+dTr1 = conv(Tr1(opts.fitIdx(1):opts.fitIdx(2)),factorial(p)/(-dt)^p * g(:,p+1));
+dTr2 = conv(Tr2(opts.fitIdx(1):opts.fitIdx(2)),factorial(p)/(-dt)^p * g(:,p+1));
+
+dTr1 = dTr1(opts.frame:(end-opts.frame));
+dTr2 = dTr2(opts.frame:(end-opts.frame));
+
+%% Calculate cross correlation
+[C,Lags] = xcorr(dTr1,dTr2);
+X = Lags*dt;
+
+%% Fit peak
+window = ceil(opts.peakWindow/2);
+[~,idx] = max(C);
+idxFit = (-window:1:window) + idx;
+
+
+fit1 = @(p,x) pseudoVoigt([p],x);
+[p,~,residual,~,~,~,J] = lsqcurvefit(fit1,opts.p00,X(idxFit),C(idxFit),[],[],optionsFit);
+ci = nlparci(p,residual,'jacobian',J); % confidence intervals
+
+%% Extract results
+deltaD = p(2);
+sigma = (ci(2,2)-ci(2,1))/4;
+end
+
+function y = pseudoVoigt(p,x)
+
+y0  = p(1);
+xc  = p(2);
+A   = exp(p(3));
+Wg  = exp(p(4));
+Wl  = exp(p(5));
+Mu  = exp(p(6));
+
+y = y0 + A*(Mu * 2/pi * Wl./(4*(x-xc).^2 + Wl^2) +...
+    (1-Mu)*(sqrt(4*log(2))./(sqrt(pi)*Wg))*exp(-(4*log(2)*(x-xc).^2)./(Wg^2)) );
+
+y=y(:);
+end
+
---
+++ runCrossCor.m
@@ -0,0 +1,66 @@
+%
+% runCrossCor.m
+%
+% Copyright (C) 2020 The University of Newcastle, Australia
+% Authors:
+%   Nicholas O'Dell <Nicholas.Odell@newcastle.edu.au>
+% Last modified: 14/01/2020
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+restoredefaultpath
+clc
+clear
+close all
+%% House Keeping
+tickFontSize = 12;
+set(0,'defaultAxesFontsize',tickFontSize)
+set(0,'defaultAxesLabelFontsize',18/tickFontSize)
+set(0,'defaultAxesTitleFontsize',25/tickFontSize)
+set(0,'defaultTextInterpreter','latex')
+set(0,'defaultLegendInterpreter', 'latex')
+set(0,'defaultAxesTickLabelInterpreter', 'latex')
+set(0, 'defaultLineLinewidth',2)
+set(0,'defaultFigureColor','w')
+%% Load Data
+addpath ./data
+addpath ./utility_functions
+load    corr_paper_data.mat
+
+%% Fit Bragg-Edge
+opts.order      = 3;            %polynomial order of Savitzky-Golay
+opts.frame      = 9;            %frame-length for Savitzky-Golay (must be Odd)
+opts.peakWindow = 20;           %window for peak fitting
+opts.range      = [4.55 4.9];   %Wavelengths of interest
+
+opts.p00 = [... %Pseudo-Voigt function params
+    15.2407     %y0
+    0.0         %xc
+    1.8534      %log(A)
+    -4.1025     %log(Wg)
+    -3.5159     %log(Wl)
+    0.0876];    %log(Mu)
+%% Fit Edges
+dd      = nan(15,1);
+sigma   = nan(15,1);
+for i =1:15
+    [dd(i),sigma(i)] = crossCorrMethod(Tr(:,i),d0_al,lambda,opts);
+end
+%% Plot Results
+figure(3); clf;
+errorbar(dist,dd,sigma)
+xlabel('Z (distance from sample center) - [mm]')
+ylabel('Shift in wavelength - [\AA]')
+title('Al\{111\} Bragg Edge')
+grid minor
