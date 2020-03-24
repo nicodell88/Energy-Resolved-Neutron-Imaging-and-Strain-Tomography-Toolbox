0102c8a79a4e5bec0d2655fbb8f0549cea18c70b
Nic O'Dell <nicodell88@gmail.com>
2020-03-24 16:58:48 +1100

    Added "edge height" code for alignment

---
+++ Alignment/getEdges.m
@@ -0,0 +1,77 @@
+function edges = getEdges(projPath,opts)
+%getEdges(projPath,opts) calculates the "pseudo edge heights" for use in
+%   sample alignment.
+% Inputs:
+%   - projPath is a relative filepath to the folder containing the
+%   projections "im_stack". XXX_proj_n.mat
+%   - opts is a structure containing
+%       opts.rangeLeft  :   a 2 element vector containing the window for
+%                           averaging the edge height before the edge
+%       opts.rangeRight :   a 2 element vector containing the window for
+%                           averaging the edge height after the edge
+%
+% Outputs:
+%   - edges is a mPix-by-nPix-by-nProj matrix where each page is contains the
+%     "edge heights" for each projection
+%
+% Copyright (C) 2020 The University of Newcastle, Australia
+% Authors:
+%   Nicholas O'Dell <Nicholas.Odell@newcastle.edu.au>
+% Last modified: 15/01/2020
+% This program is licensed under GNU GPLv3, see LICENSE for more details.
+
+% Grab data files containing "proj"
+% pathStr = fullfile(projPath,'*proj*.mat');
+% dataFiles = dir(pathStr);
+% assert(length(dataFiles)>=1,'Expected more than zero projections to exist!');
+OB = load(opts.OB,'tof','im_stack','ntrigs');
+ntrigs_rep_OB = reshape(OB.ntrigs,1,1,numel(OB.ntrigs)).*ones(size(OB.im_stack,1),size(OB.im_stack,2),numel(OB.ntrigs));
+OB.im_stack = OB.im_stack./ntrigs_rep_OB;
+
+fname = sprintf(opts.fmt,opts.proj_idx(1));
+dataFiles = dir(fullfile(projPath,fname));
+
+% Determine data size
+matObj = matfile(fullfile(dataFiles(1).folder,dataFiles(1).name));
+
+nPixRow = size(matObj,'im_stack',1);
+nPixCol = size(matObj,'im_stack',2);
+nProj     = length(opts.proj_idx);
+
+% Initialise edges
+edges = nan(nPixRow,nPixCol,nProj);
+
+wh = updateWaitbar();
+for i = 1:nProj
+    [wh,flag] = updateWaitbar(i,nProj,wh);
+    if flag
+        warning('User cancelled operation');
+        return
+    end
+    
+    str = sprintf(opts.fmt,opts.proj_idx(i));
+    
+    Proj = load(fullfile(projPath,str),'im_stack','tof','ntrigs');
+    ntrigs_rep_Proj = reshape(Proj.ntrigs,1,1,numel(Proj.ntrigs)).*ones(size(Proj.im_stack,1),size(Proj.im_stack,2),numel(Proj.ntrigs));
+    Proj.im_stack = Proj.im_stack./ntrigs_rep_Proj;
+    
+    [~,leftIdx]    = min((Proj.tof(:).'-opts.rangeLeft(:)).^2,[],2);
+    [~,rightIdx]   = min((Proj.tof(:).'-opts.rangeRight(:)).^2,[],2);
+    
+    Iright  = mean(Proj.im_stack(:,:,rightIdx(1):rightIdx(2)),3);
+    ILeft   = mean(Proj.im_stack(:,:,leftIdx(1):leftIdx(2)),3);
+    I0left  = mean(OB.im_stack(:,:,leftIdx(1):leftIdx(2)),3);
+    I0right = mean(OB.im_stack(:,:,rightIdx(1):rightIdx(2)),3);
+    
+    edges(:,:,i) = log(Iright) -log(I0right) + log(I0left) - log(ILeft);
+    
+    
+%     before_edge = mean(Proj.im_stack(:,:,leftIdx(1) :leftIdx(2) ),3);
+%     after_edge  = mean(Proj.im_stack(:,:,rightIdx(1):rightIdx(2)),3);
+    
+%    edge = log(mean(Tr(:,:,opts.endIdx(1):opts.endIdx(2)),3))-log(mean(Tr(:,:,opts.startIdx(1):opts.startIdx(2)),3));
+%     edges(:,:,i) = log(after_edge) - log(before_edge);
+    
+end
+delete(wh);
+end
---
+++ Alignment/runSaveEdges.m
@@ -0,0 +1,63 @@
+%
+% runSaveEdges.m
+%
+% Copyright (C) 2020 The University of Newcastle, Australia
+% Authors:
+%   Nicholas O'Dell <Nicholas.Odell@newcastle.edu.au>
+% Last modified: 15/01/2020
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+restoredefaultpath
+clc
+clear
+close all
+%% House Keeping
+tickFontSize = 12;
+set(0,'defaultAxesFontsize',tickFontSize)
+set(0,'defaultAxesLabelFontsize',18/tickFontSize)
+set(0,'defaultAxesTitleFontsize',25/tickFontSize)
+set(0,'defaultTextInterpreter','latex')
+set(0,'defaultLegendInterpreter', 'latex')
+set(0,'defaultAxesTickLabelInterpreter', 'latex')
+set(0, 'defaultLineLinewidth',2)
+set(0,'defaultFigureColor','w')
+%%
+addpath ../utility_functions
+%%
+opts.rangeLeft  = [0.01825 0.01846];        %range for averaging on the left of the edge (same units as projX/tof)
+opts.rangeRight = [0.01846 0.01912];        %range for averaging on the right of the edge (same units as projX/tof)
+opts.fmt        = 'Cube1__proj%d_idx.mat';  %data file nmae spec
+opts.proj_idx   = [0:69];                   %projections to be used for sample alignment
+projPath = uigetdir('/Users/megatron/Dropbox/Strain Tomography 2020/J-PARC 2019 Working Folder/data/cube_plug');
+opts.OB  = '/Users/megatron/Dropbox/Strain Tomography 2020/J-PARC 2019 Working Folder/data/open_beam/open_beam_11ksec.mat';
+tic
+edges = getEdges(projPath,opts);
+toc
+%%
+figure(1)
+clf
+xlabel('hi')
+for i = 1:size(edges,3)
+    imagesc(edges(:,:,i))
+    xlabel('X - [pixels]')
+    ylabel('Y - [pixels]')
+    title('Edge heights')
+    pause(1)
+end
+return
+%% Edges
+fname = sprintf('edges_%s',datestr(now,'ddmmyy_hhMMss'))
+save(fname,'edges')
+
