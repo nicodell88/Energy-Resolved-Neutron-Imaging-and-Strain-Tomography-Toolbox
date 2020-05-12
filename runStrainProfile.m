52fadac1c108818039c300890da4db7a3b83f996
Nic O'Dell <nicodell88@gmail.com>
2020-05-12 11:33:21 +1000

    2D data

--- runStrainProfile.m
+++ runStrainProfile.m
@@ -1,5 +1,5 @@
 %
-% runStrainProfileExample.m 
+% runStrainProfileExample.m
 %
 % Copyright (C) 2020 The University of Newcastle, Australia
 % Authors:
@@ -10,12 +10,12 @@
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % This program is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -39,8 +39,8 @@ set(0,'defaultFigureColor','w')
 
 
 %% Options
-opts.ObFile     = '/Users/megatron/Dropbox/Strain Tomography 2020/J-PARC 2020 Working Folder/Data/OpenBeam/OpenBeamImg__proj001_idx.mat';   %Open Beam File location
-opts.ProjFile   = '/Users/megatron/Dropbox/Strain Tomography 2020/J-PARC 2020 Working Folder/Data/SteelCube/SteelCube__proj001_idx.mat';    %Projection File location
+opts.ObFile     = '/Users/megatron/Dropbox/Strain Tomography 2020/LaTeX/ToolboxPaper/matlab/preprocessed/JPARC2018_OB_proj_003.mat';   %Open Beam File location
+opts.ProjFile   = '/Users/megatron/Dropbox/Strain Tomography 2020/LaTeX/ToolboxPaper/matlab/preprocessed/JPARC2018_proj_000.mat';    %Projection File location
 
 
 OB      = load(opts.ObFile);    %Load open beam
@@ -50,25 +50,30 @@ Proj    = load(opts.ProjFile);  %Load projection
 % opts.supplyMask = 'gui';
 opts.supplyMask = 'auto';
 
-opts.rangeRight = [0.0190 0.0198];  %Range for averaging on right of edge for guessing mask
-opts.rangeLeft  = [0.0175 0.0185];  %Range for averaging on left of edge for guessing mask
+opts.rangeRight = [0.0186 0.0188];  %Range for averaging on right of edge for guessing mask
+opts.rangeLeft  = [0.0175 0.01835];  %Range for averaging on left of edge for guessing mask
 % opts.d0         =  0.018821361897845;           %Unstrained tof/wl
-opts.d0         =  0.018821361897845 + 3e-6;           %Unstrained tof/wl
+opts.d0         =  0.01845;           %Unstrained tof/wl
 %% PROFILE OPTIONS
-opts.pixelCentre = 250;
-opts.nWidth     = 5;    %
-opts.nPix       = 50;   %averages over (2*nPix+1)-by-(2*nWidth+1)
-opts.nRes      = 2;
+opts.pixelCentre = 150;
+opts.nWidth     = 15;    %
+opts.nPix       = 80;   %averages over (2*nPix+1)-by-(2*nWidth+1)
+opts.nRes       = 1;
+%% IMAGE OPTIONS
+% opts.pixelCentre = 140;
+% opts.nWidth     = 10;    %
+% opts.nPix       = 18;   %averages over (2*nPix+1)-by-(2*nWidth+1)
+% opts.nRes       = 18;
 %%
 opts.Thresh     = 0.1;             %More than 90% of a macro-pixel must be within the mask before a bragg edge will be fit to it
 opts.maskThresh = 0.02;             %Threshold edge height for mask
 
 
-opts.AveDir      = 'horz'; %'horz'
+opts.AveDir      = 'vert'; %'horz'
 
 % Insert Bragg Edge Fitting Options here
-opts.BraggOpts.startRange = [0.0175 0.0180];  %Fitting left side of edge
-opts.BraggOpts.endRange   = [0.019 0.0195];   %Fitting right side of edge
+opts.BraggOpts.startRange = [0.0182 0.01835];  %Fitting left side of edge
+opts.BraggOpts.endRange   = [0.0186 0.0188];   %Fitting right side of edge
 
 opts.BraggOpts.GPscheme     = 'hilbertspace';             %Fitting algorithm
 opts.BraggOpts.method = 'gp';
@@ -76,8 +81,8 @@ opts.BraggOpts.covfunc = 'M52';        %Sets the GP covariance function (current
 opts.BraggOpts.optimiseHP = 'all';
 
 opts.BraggOpts.sigma0     = 0.01;        %Initial guess for gaussian broadening term
-opts.BraggOpts.tau0       = 0.01;        %Initial guess for exponential decay term
-opts.t_hkl0     = 0.01888;     %Initial guess for edge location
+opts.BraggOpts.tau0       = 0.01 ;        %Initial guess for exponential decay term
+opts.t_hkl0     = 0.01845;     %Initial guess for edge location
 
 opts.BraggOpts.plot   = false;             %plot results along the way
 opts.BraggOpts.a00    = 0.5;          %Initial guess for a0
@@ -97,12 +102,14 @@ opts.BraggOpts.Par = true;
 opts.figNum = 1;
 % tic
 [StrainProfile,SigmaProfile,opts]= makeStrainProfile(OB,Proj,opts);
-figure(1)
+figure(2)
 clf
-plot(StrainProfile)
+% plot(StrainProfile)
+errorbar(StrainProfile - nanmean(StrainProfile),2*SigmaProfile)
 % toc
 %% Plot Strain Image
-% plotStrainImage(StrainProfile,SigmaProfile,opts)
+% [StrainImage,SigmaImage,opts]= makeStrainImage(OB,Proj,opts);
+% plotStrainImage(StrainImage,SigmaImage,opts)
 %% Save Figure
 % msg = sprintf('strainImage_%s',datestr(now,'yy_mm_dd_hh_MM_ss'));
 % saveas(gcf,msg,'png')
